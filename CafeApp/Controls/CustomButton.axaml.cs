fc155691f9597f6a5239f5d6b9c6cc37f31ff402
Ksenia <146197706+Merkuryyi@users.noreply.github.com>
2025-10-28 13:10:25 +0700

    Initial commit

---
+++ CafeApp/Controls/CustomButton.axaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace CafeApp.Controls
+{
+    public partial class CustomButton : UserControl
+    {
+        public string ButtonText
+        {
+            get => MainButton.Content?.ToString() ?? "";
+            set => MainButton.Content = value;
+        }
+
+        public event RoutedEventHandler Click;
+
+        public CustomButton()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            Click?.Invoke(this, e);
+        }
+    }
+}
