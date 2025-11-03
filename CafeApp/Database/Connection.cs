d3e8f997dce8954e66a5af50b06cd0ad16b6820d
Ksenia <146197706+Merkuryyi@users.noreply.github.com>
2025-11-03 22:15:15 +0700

    начало работы с бд

---
+++ CafeApp/Database/Connection.cs
@@ -0,0 +1,12 @@
+﻿namespace CafeApp.Models
+{
+    public class AppConfig
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        
+        public getConnection()
+        {
+            ConnectionString = "Host=localhost;Port=5432;Database=Cafe;Username=postgres;Password=;";
+        }
+    }
+}
